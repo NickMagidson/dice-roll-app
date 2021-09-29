536692ce1d37a6d4dcc78c7ceab484390f055226
Nick <nickmagi24@gmail.com>
2021-09-29 16:42:44 -0400

    d20 function

--- script.js
+++ script.js
@@ -3,8 +3,19 @@
 
 
 // Generate random num between 1 and 20
+function diceRoll() {
+    return Math.floor(Math.random() * (20 - 1) + 1);
+}
+
+
+
+// Show results in center of #dice-container
 
 
 
 
-//Show results in center of #dice-container
+// Tests
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
+console.log(diceRoll());
